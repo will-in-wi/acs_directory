59dcb99da73f244660f9d523d04e269865957af2
William Johnston <wjohnston@mpr.org>
2014-05-26 10:56:11 -0500

    Add stubbing so that tests can run.

--- spec/lib/acs/individual_spec.rb
+++ spec/lib/acs/individual_spec.rb
@@ -5,13 +5,15 @@ require 'spec_helper'
 
 describe ACS::Individual do
   describe '.find' do
+
+    before { stub_request(:get, "https://testuser:testpassword@secure.accessacs.com/api_accessacs_mobile/v2/123456/individuals/2").
+             to_return(:status => 200, :body => IO.read(Rails.root.join('spec', 'fixtures', 'individuals', 'person1.json')), :headers => { 'Content-Type' => 'application/json; charset=utf-8' }) }
+
     it 'works in best case' do
-      pending
-      WebMock.allow_net_connect!
-      i = ACS::Individual.find 254
-      expect(i.last_name).to eql('SomeName')
-      expect(i.indv_id).to eql(254)
-      expect(i.prim_family).to eql(1094)
+      i = ACS::Individual.find 2
+      expect(i.last_name).to eql('Doe')
+      expect(i.indv_id).to eql(2)
+      expect(i.prim_family).to eql(1001)
     end
   end
 
