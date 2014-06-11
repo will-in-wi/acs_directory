ceff112961d59137b6d2ea7ae3afaf3f881c3427
William Johnston <wjohnston@mpr.org>
2014-06-11 07:09:01 -0500

    Destroy records in join tables on delete.

--- app/models/individual.rb
+++ app/models/individual.rb
@@ -2,13 +2,13 @@ class Individual < ActiveRecord::Base
   self.primary_key = 'id'
 
   has_many :email_addresses, through: :email_addresses_individuals
-  has_many :email_addresses_individuals
+  has_many :email_addresses_individuals, dependent: :destroy
 
   has_many :addresses, through: :addresses_individuals
-  has_many :addresses_individuals
+  has_many :addresses_individuals, dependent: :destroy
 
   has_many :phone_numbers, through: :individuals_phone_numbers
-  has_many :individuals_phone_numbers
+  has_many :individuals_phone_numbers, dependent: :destroy
 
   validates_uniqueness_of :id
 end
