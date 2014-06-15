bc77b7f986a711681c62f9b3129faa3e586c16eb
William Johnston <wjohnston@mpr.org>
2014-06-15 14:51:40 -0500

    Add show action

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -2,4 +2,8 @@ class FamiliesController < ApplicationController
   def index
     @individuals = Individual.where(family_position: 'Head').order(last_name: :asc)
   end
+
+  def show
+    @family = Individual.where(family_id: params[:id])
+  end
 end
