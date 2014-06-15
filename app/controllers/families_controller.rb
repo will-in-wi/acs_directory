5cf6303fe0bc5ee4913c07ee5b2d912657d6e8ae
William Johnston <wjohnston@mpr.org>
2014-06-14 20:36:40 -0500

    Rename to families

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,4 +1,4 @@
-class IndividualsController < ApplicationController
+class FamiliesController < ApplicationController
   def index
     @individuals = Individual.where(family_position: 'Head').order(last_name: :asc)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :individuals, only: [:index, :show]
+  resources :families, only: [:index, :show]
 
   # You can have the root of your site routed with "root"
-  root 'individuals#index'
+  root 'families#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
